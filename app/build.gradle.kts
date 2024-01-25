4038a37725a1ce81a6447fdde487d4311523b149
sengokyu <sengokyu+gh@gmail.com>
2024-01-25 13:07:10 +0900

    Upddate build.gradle
    Downgrade support library version.
    Bump minSdk version 9 -> 10
    (Android 2.3.3 support minSdk 10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.ir.backend.js.compile
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -5,12 +7,13 @@ plugins {
 
 android {
     namespace = "win.hile.captureandupload"
-    compileSdk = 34
+    compileSdk = 10
 
     defaultConfig {
         applicationId = "win.hile.captureandupload"
-        minSdk = 9
-        targetSdk = 9
+        minSdk = 10
+        //noinspection ExpiredTargetSdkVersion
+        targetSdk = 10
         versionCode = 1
         versionName = "0.1"
 
@@ -40,7 +43,9 @@ dependencies {
 //    implementation("androidx.core:core-ktx:1.10.1")
 //    implementation("androidx.appcompat:appcompat:1.6.1")
 //    implementation("com.google.android.material:material:1.9.0")
-    implementation("com.android.support:support-v4:28.0.0")
+    //noinspection GradleCompatible
+    implementation("com.android.support:support-v4:25.4.0")
+    implementation("com.android.support:appcompat-v7:25.4.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
